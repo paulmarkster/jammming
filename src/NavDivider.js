4cd590ceabcee1e4d0a76e4b452d146c5263d14e
Paul Asselin <passelin61@gmail.com>
2024-04-07 16:49:20 -0400

    Added all nav components and improved nav margins/padding.

---
+++ src/NavDivider.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import './NavDivider.css';
+
+export default function NavDivider() {
+    return (
+        <div className='nav-divider'></div>
+    );
+}
--- src/NavDrawer.js
+++ src/NavDrawer.js
@@ -1,19 +1,29 @@
 import React from 'react';
 import './NavDrawer.css';
-import HomeOutlined from '@mui/icons-material/HomeOutlined';
-import Radio from '@mui/icons-material/Radio';
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import RadioIcon from '@mui/icons-material/Radio';
 import QueueMusicIcon from '@mui/icons-material/QueueMusic';
+import AddToQueueIcon from '@mui/icons-material/AddToQueue'
+import PersonOutlineIcon from '@mui/icons-material/PersonOutline'
 import NavHeader from './NavHeader';
 import NavItem from './NavItem';
+import NavDivider from './NavDivider';
 
 export function NavDrawer() {
     return (
         <div className='nav-drawer'>
-            <NavHeader text='Jammming' />
-            <NavHeader text='Pages' />
-            <NavItem icon={<HomeOutlined />} text='Welcome' />
-            <NavItem icon={<QueueMusicIcon />} text='Add to Queue' />
-            <NavItem icon={<Radio />} text='Player' />                        
+            <div className='nav-drawer-inner'>
+                <NavHeader text='Pages' />
+                <NavItem icon={<HomeOutlinedIcon />} text='Welcome' />
+                <NavItem icon={<AddToQueueIcon />} text='Add to Queue' />
+                <NavItem icon={<RadioIcon />} text='Music Player' />  
+                <NavDivider />
+                <NavHeader text= 'Appearance' />
+                <NavItem icon={<QueueMusicIcon />} text='Queue' />
+                <NavDivider />
+                <NavHeader text='Settings' />
+                <NavItem icon={<PersonOutlineIcon />} text='<User>' />
+            </div>                      
         </div>
     );
 }
