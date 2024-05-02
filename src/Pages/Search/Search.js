2960979d9a3d61f8c1baa6110ce9db4def0d2c86
Paul Asselin <passelin61@gmail.com>
2024-05-02 09:04:00 -0400

    Moved search query from Search to SearchBar.

--- src/Pages/Search/Search.js
+++ src/Pages/Search/Search.js
@@ -1,49 +1,20 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styles from './Search.module.css';
 import SearchBar from './SearchBar';
 import SearchDivider from './SearchDivider';
 import SearchCard from './SearchCard';
-import { getRefreshTokens } from '../../Spotify/spotifyOAuth';
-import { spotifySearch } from '../../Spotify/spotifyAPI';
 
 export default function Search() {
 
-  // Create a state for search query returned by the SearchBar child component.
-  const [query, setQuery] = useState(null);
-  // Create a state for search query results to initiate the render of the returned search list.
-  const [queryResult, setQueryResult] = useState([]);
-
-  useEffect(() => {
-
-    // Check to see if a query is available. 
-    // This protects against initiating an empty search request to the Spotify server on initial component mount.
-    if (query) {
-
-      // Check to ensure there is a user logged into Spotify.
-      if (sessionStorage.getItem('client_id')) {
-
-        // Refresh tokens (only if required) and fetch search results from Spotify.
-        getRefreshTokens().then(() => {
-          spotifySearch(query).then((queryResponse) => {
-            setQueryResult(queryResponse.tracks.items);
-          });
-        });
-      } else {
-        alert('Login prior to initiating searches.');
-      }
-    }
-    return () => {
-      // Nullify the query state to avoid infinite search requests on a given query.
-      setQuery(null);
-    }
-  }, [query, queryResult])
+  // State for search results array initially returned by SearchBar, and manipulated later in SearchCard.
+  const [query, setQuery] = useState([]);
    
   return (
     <div className={styles.container}>
       <SearchBar setQuery={setQuery} />
       <SearchDivider />
       <div className={styles.search}>
-        {queryResult.map(item => (
+        {query.map(item => (
           <SearchCard key={item.id} track={item} />
         ))}
       </div>
--- src/Pages/Search/SearchBar.js
+++ src/Pages/Search/SearchBar.js
@@ -1,14 +1,29 @@
 import React from 'react';
 import styles from './SearchBar.module.css';
 import SearchIcon from '@mui/icons-material/Search';
+import { getRefreshTokens } from '../../Spotify/spotifyOAuth';
+import { spotifySearch } from '../../Spotify/spotifyAPI';
 
 export default function SearchBar({setQuery}) {
 
-  const handleSubmit = (event) => {
-    if (event.key === 'Enter') {
-      const artist = encodeURIComponent(event.target.value);
+  const handleSubmit = (e) => {
+    if (e.key === 'Enter') {
+
+      const artist = encodeURIComponent(e.target.value);
       const uri = encodeURI(`https://api.spotify.com/v1/search?q=${artist}&type=track`);
-      setQuery(uri);
+
+      // Check to ensure there is a user logged into Spotify.
+      if (sessionStorage.getItem('client_id')) {
+
+        // Refresh tokens (only if required) and fetch search results from Spotify.
+        getRefreshTokens().then(() => {
+          spotifySearch(uri).then((queryResponse) => {
+            setQuery(queryResponse.tracks.items);
+          });
+        });
+      } else {
+        alert('Login prior to initiating searches.');
+      }
     };
   }
 
