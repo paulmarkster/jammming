19302dfb0884ac45ed69a7ae24774233bda5073d
Paul Asselin <passelin61@gmail.com>
2024-05-03 09:48:26 -0400

    Added try/catch to all Spotify API calls.

--- src/Spotify/spotifyAPI.js
+++ src/Spotify/spotifyAPI.js
@@ -1,32 +1,44 @@
 export async function getProfile() {
 
-  // Fetch user's profile.
-  const accessToken = sessionStorage.getItem('access_token'); 
-  const response = await fetch('https://api.spotify.com/v1/me', {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + accessToken }
-  });
-  return await response.json();
+  try {
+    // Fetch user's profile.
+    const accessToken = sessionStorage.getItem('access_token'); 
+    const response = await fetch('https://api.spotify.com/v1/me', {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function spotifySearch(query) {
 
-  // Fetch search query results.
-  const accessToken = sessionStorage.getItem('access_token');
-  const response = await fetch(query, {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + accessToken }
-  });
-  return await response.json();
+  try {
+    // Fetch search query results.
+    const accessToken = sessionStorage.getItem('access_token');
+    const response = await fetch(query, {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function getUsersQueue() {
 
-  // Fetch user's play queue.
-  const accessToken = sessionStorage.getItem('access_token'); 
-  const response = await fetch('https://api.spotify.com/v1/me/player/queue', {
-    method: 'GET',
-    headers: { 'Authorization': 'Bearer ' + accessToken }
-  });
-  return await response.json();
+  try {
+    // Fetch user's play queue.
+    const accessToken = sessionStorage.getItem('access_token'); 
+    const response = await fetch('https://api.spotify.com/v1/me/player/queue', {
+      method: 'GET',
+      headers: { 'Authorization': 'Bearer ' + accessToken }
+    });
+    return await response.json();
+  } catch (error) {
+    console.error(error);
+  }
 }
--- src/Spotify/spotifyOAuth.js
+++ src/Spotify/spotifyOAuth.js
@@ -2,105 +2,121 @@
 
 export async function getAuth() {
 
-  const generateRandomString = (length) => {
-    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-    const values = crypto.getRandomValues(new Uint8Array(length));
-    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  try {
+    const generateRandomString = (length) => {
+      const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+      const values = crypto.getRandomValues(new Uint8Array(length));
+      return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+    }
+    
+    const codeVerifier  = generateRandomString(64);
+    sessionStorage.setItem('code_verifier', codeVerifier);
+
+    const sha256 = async (plain) => {
+      const encoder = new TextEncoder()
+      const data = encoder.encode(plain)
+      return window.crypto.subtle.digest('SHA-256', data)
+    }
+
+    const base64encode = (input) => {
+      return btoa(String.fromCharCode(...new Uint8Array(input)))
+      .replace(/=/g, '')
+      .replace(/\+/g, '-')
+      .replace(/\//g, '_');
+    }
+
+    const hashed = await sha256(codeVerifier)
+    const codeChallenge = base64encode(hashed);
+    const clientId = sessionStorage.getItem('client_id');
+    const redirectUri = 'http://localhost:3000';
+    const scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state';
+    const authUrl = new URL("https://accounts.spotify.com/authorize")
+    const params =  {
+      response_type: 'code',
+      client_id: clientId,
+      scope,
+      code_challenge_method: 'S256',
+      code_challenge: codeChallenge,
+      redirect_uri: redirectUri,
+    }
+
+    authUrl.search = new URLSearchParams(params).toString();
+    window.location.href = authUrl.toString();
+
+  } catch (error) {
+    console.error(error);
   }
-  
-  const codeVerifier  = generateRandomString(64);
-  sessionStorage.setItem('code_verifier', codeVerifier);
-
-  const sha256 = async (plain) => {
-    const encoder = new TextEncoder()
-    const data = encoder.encode(plain)
-    return window.crypto.subtle.digest('SHA-256', data)
-  }
-
-  const base64encode = (input) => {
-    return btoa(String.fromCharCode(...new Uint8Array(input)))
-    .replace(/=/g, '')
-    .replace(/\+/g, '-')
-    .replace(/\//g, '_');
-  }
-
-  const hashed = await sha256(codeVerifier)
-  const codeChallenge = base64encode(hashed);
-  const clientId = sessionStorage.getItem('client_id');
-  const redirectUri = 'http://localhost:3000';
-  const scope = 'user-read-private user-read-email user-read-currently-playing user-read-playback-state';
-  const authUrl = new URL("https://accounts.spotify.com/authorize")
-  const params =  {
-    response_type: 'code',
-    client_id: clientId,
-    scope,
-    code_challenge_method: 'S256',
-    code_challenge: codeChallenge,
-    redirect_uri: redirectUri,
-  }
-
-  authUrl.search = new URLSearchParams(params).toString();
-  window.location.href = authUrl.toString();
-
 }
 
 export async function getTokens(code) {
-  const clientId = sessionStorage.getItem('client_id');
-  const redirectUri = 'http://localhost:3000';
-  
-  // stored in the previous step
-  const codeVerifier = sessionStorage.getItem('code_verifier');
-  
-  const payload = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded',
-    },
-    body: new URLSearchParams({
-      client_id: clientId,
-      grant_type: 'authorization_code',
-      code,
-      redirect_uri: redirectUri,
-      code_verifier: codeVerifier,
-    }),
-  }
-  
-  const body = await fetch('https://accounts.spotify.com/api/token', payload);
-  const response = await body.json();
-  
-  const { access_token, refresh_token, expires_in } = response;
-  sessionStorage.setItem('access_token', access_token);
-  sessionStorage.setItem('refresh_token', refresh_token);
-  const now = new Date();
-  sessionStorage.setItem('expires', new Date(now.getTime() + (expires_in * 1000)));
-}
 
-export async function getRefreshTokens() {
-
-  if (new Date().getTime() >= sessionStorage.getItem('expires')) {
-    const refreshToken = sessionStorage.getItem('refresh_token');
+  try {
     const clientId = sessionStorage.getItem('client_id');
-    const url = "https://accounts.spotify.com/api/token";
-
+    const redirectUri = 'http://localhost:3000';
+    
+    // stored in the previous step
+    const codeVerifier = sessionStorage.getItem('code_verifier');
+    
     const payload = {
       method: 'POST',
       headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
       },
       body: new URLSearchParams({
-        grant_type: 'refresh_token',
-        refresh_token: refreshToken,
-        client_id: clientId
+        client_id: clientId,
+        grant_type: 'authorization_code',
+        code,
+        redirect_uri: redirectUri,
+        code_verifier: codeVerifier,
       }),
     }
-
-    const body = await fetch(url, payload);
+    
+    const body = await fetch('https://accounts.spotify.com/api/token', payload);
     const response = await body.json();
-
+    
     const { access_token, refresh_token, expires_in } = response;
     sessionStorage.setItem('access_token', access_token);
     sessionStorage.setItem('refresh_token', refresh_token);
     const now = new Date();
     sessionStorage.setItem('expires', new Date(now.getTime() + (expires_in * 1000)));
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function getRefreshTokens() {
+
+  try {
+
+    if (new Date().getTime() >= sessionStorage.getItem('expires')) {
+      const refreshToken = sessionStorage.getItem('refresh_token');
+      const clientId = sessionStorage.getItem('client_id');
+      const url = "https://accounts.spotify.com/api/token";
+
+      const payload = {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: new URLSearchParams({
+          grant_type: 'refresh_token',
+          refresh_token: refreshToken,
+          client_id: clientId
+        }),
+      }
+
+      const body = await fetch(url, payload);
+      const response = await body.json();
+
+      const { access_token, refresh_token, expires_in } = response;
+      sessionStorage.setItem('access_token', access_token);
+      sessionStorage.setItem('refresh_token', refresh_token);
+      const now = new Date();
+      sessionStorage.setItem('expires', new Date(now.getTime() + (expires_in * 1000)));
+    }
+  
+  } catch (error) {
+    console.error(error);
   }
 }
